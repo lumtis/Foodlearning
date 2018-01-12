c189ab780850db5c5532755e89a23729f22d38e8
Lucas Bertrand <lucas.bertrand@ensimag.grenoble-inp.fr>
2018-01-12 11:12:13 +0100

    Scripts update

--- scripts_1/init_kafka.sh
+++ scripts_1/init_kafka.sh
@@ -1,1 +1,2 @@
 # Change port in kafka config
+echo "port=9093" >> /opt/Kafka/kafka_2.10-0.10.0.1/config/server.properties
--- scripts_1/launch_akka.sh
+++ scripts_1/launch_akka.sh
@@ -1,4 +1,6 @@
 echo "START SCRIPT LAUNCH AKKA\n" > ~/logs.txt
 
+cd ./Foodlearning
+sbt run
 
 echo 'SCRIPT LAUNCH AKKA EXECUTED' >> ~/logs.txt
